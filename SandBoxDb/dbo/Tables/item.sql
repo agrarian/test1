6f2d6da716c9e9f74b8dadae70f6702042c3ef45
agrarian <tcarnes@reedtech.com>
2015-02-17 14:38:15 -0500

    initial commit

---
+++ SandBoxDb/dbo/Tables/item.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[item] (
+    [ItemId]      INT          IDENTITY (1, 1) NOT NULL,
+    [Description] VARCHAR (50) NULL,
+    [OrderId]     INT          NOT NULL,
+    CONSTRAINT [PK_item] PRIMARY KEY CLUSTERED ([ItemId] ASC),
+    CONSTRAINT [FK_item_order] FOREIGN KEY ([OrderId]) REFERENCES [dbo].[order] ([OrderId])
+);
+
---
+++ SandBoxDb/dbo/Tables/order.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[order] (
+    [OrderId]   INT        IDENTITY (1, 1) NOT NULL,
+    [Name]      NCHAR (10) NULL,
+    [OrderDate] DATETIME   NULL,
+    CONSTRAINT [PK_order] PRIMARY KEY CLUSTERED ([OrderId] ASC)
+);
+
